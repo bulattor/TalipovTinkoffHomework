7ca504367f3c0007b0aeb9bef0f792d9311f4056
bulattor <bulattor@yandex.ru>
2022-03-24 16:45:47 +0300

    task4 upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,17 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("junit:junit:4.13.2")
+    implementation("io.mockk:mockk-dsl:1.12.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testImplementation("io.mockk:mockk:1.12.3")
+}
+
+tasks{
+    test{
+        useJUnitPlatform()
+    }
 }
