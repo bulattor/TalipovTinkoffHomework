b063d50b8a02389762506c03995b744899c86656
bulattor <bulattor@yandex.ru>
2022-04-21 23:42:25 +0300

    task8 init

--- src/main/kotlin/ru/tinkoff/fintech/task5/GraphicsCard.kt
+++
@@ -1,9 +0,0 @@
-package ru.tinkoff.fintech.task5
-
-data class GraphicsCard (
-    val name: String,
-    val brand: String,
-    val chipset: String,
-    val price: Double,
-    val memory: Int,
-)
